d39526189144eb16242dcd01601f515c54e3ce58
WinSukon <jakrinsukon@gmail.com>
2023-11-12 16:06:59 +0700

    add menu item

--- frontend/src/components/Menus.tsx
+++ frontend/src/components/Menus.tsx
@@ -1,12 +1,30 @@
-const Meuns = () => {
+import Link from "next/link";
+import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
+import BookIcon from '@mui/icons-material/Book';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+
+const Menus = () => {
     return (  
         <div className="flex w-[5%] flex-col">
-            <div>asavsav</div>
-            <div>asavsav</div>
-            <div>asavsav</div>
-            <div>asavsav</div>
+          
+            <Link href="/restaurants" className="flex flex-row">
+                <RestaurantMenuIcon></RestaurantMenuIcon>
+                <div className="flex">Restaurants</div>
+            </Link>
+
+            <Link href="/mybooking" className="flex flex-row">
+                <BookIcon></BookIcon>
+                <div className="flex">Reservation</div>
+            </Link>
+
+            <Link href="/Booking" className="flex flex-row">
+                <AddBoxIcon></AddBoxIcon>
+                <div className="flex">Booking</div>
+            </Link>
+
+
         </div>
     );
 }
  
-export default Meuns;
+export default Menus;

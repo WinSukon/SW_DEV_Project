17b5fe86a70d90563c195871e57ff2e87e9465f0
ChawinNg <ik.chawin@gmail.com>
2023-11-17 18:56:23 +0700

    menuV2

--- frontend/src/components/Menus.tsx
+++ frontend/src/components/Menus.tsx
@@ -1,47 +1,40 @@
 import Image from "next/image";
 import TopMenuItem from "./TopMenuItem";
-const Menus = () => {
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Link from "next/link";
+export default async function Menus(){
+
+    const session = await getServerSession(authOptions)
     return (  
-        // <div className="flex w-[15%] flex-col p-5 bg-[#FFCE50] decoration-transparent">
-        //     <div className="bottom">
-        //         <Link href="/restaurants" className="flex flex-row">
-        //             <RestaurantMenuIcon></RestaurantMenuIcon>
-        //             <div>Restaurants</div>
-        //         </Link>
-
-        //         <Link href="/booking" className="flex flex-row">
-        //             <BookIcon></BookIcon>
-        //             <div>Reservation</div>
-        //         </Link>
-
-        //         <Link href="/Booking" className="flex flex-row">
-        //             <AddBoxIcon></AddBoxIcon>
-        //             <div>Booking</div>
-        //         </Link>
-        //     </div>
-        
-
-        //     <div className="bottom-0">
-        //         <Link href="" className="flex flex-row">
-        //             <LoginIcon></LoginIcon>
-        //             <div>Login</div>
-        //         </Link>
-
-        //         <Link href="" className="flex flex-row">
-        //             <LogoutIcon></LogoutIcon>
-        //             <div>Logout</div>
-        //         </Link>
-        //     </div>
-        // </div>
         <div className="h-16 bg-[#FFCE50] fixed z-50 inset-x-0 top-0 flex flex-row justify-center shadow-lg">
             <TopMenuItem title='Booking' pageRef='/booking'/>
             <div className="border border-black my-2"></div>
             <TopMenuItem title='Restaurants' pageRef='/restaurants'/>
             <div className="border border-black my-2"></div>
             <TopMenuItem title='Reservation' pageRef='/mybooking'/>
-            
+            {
+                
+                session? <Link href="/api/auth/signout">
+                    <div className='w-30 h-[50px] center my-[6px] font-sans text-xl 
+        text-black font-bold rounded-md hover:bg-amber-400 pt-[10px]  px-2 flex item-center absolute right-5'>
+                    Sign-Out of {session.user?.name}
+                    </div>
+                    </Link>
+                : <Link href="/api/auth/signin">
+                    <div className='w-30 h-[50px] center my-[6px] font-sans text-xl 
+        text-black font-bold rounded-md hover:bg-amber-400 pt-[10px]  px-4 flex item-center absolute right-5'>
+                    Sign-In
+                    </div>
+                </Link>
+            }
+            <div>
+            <Link className='w-30 h-[50px] center my-[6px] font-sans text-xl 
+        text-black font-bold rounded-md hover:bg-amber-400 pt-[10px]  px-4 flex item-center absolute left-5' href='/'>
+                Home
+            </Link>
+            </div>
         </div>
     );
 }
  
-export default Menus;

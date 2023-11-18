a5b5062c98c0f624c001437bf297011493c6a59e
WinSukon <jakrinsukon@gmail.com>
2023-11-18 13:37:25 +0700

    add nextauthprovider for session to be accessible by client comp

--- frontend/src/libs/getRestaurants.tsx
+++ frontend/src/libs/getRestaurants.tsx
@@ -1,6 +1,4 @@
 export default async function  getRestaurants() {
-
-    await new Promise((resolve)=>setTimeout(resolve,5000))
     
     const response = await fetch("http://localhost:5000/api/v1/restaurants",{next : {tags:['hoss']}})
     if(!response.ok){
